7a83fa1968717745af59887731033edc033ec036
JoaoPaulo <jpogd@hotmail.com>
2023-10-13 17:04:50 -0300

    first commit

---
+++ Media.java
@@ -0,0 +1,20 @@
+public class Media {
+    public static void main(String args[]){
+
+        double nota1, nota2, nota3, nota4, nota5, nota6;
+
+        nota1 = 9;
+        nota2 = 8.5;
+        nota3 = 7.5;
+        nota4 = 10;
+
+        nota5 = nota1+nota2+nota3+nota4;
+
+        nota6 = nota5/4;
+        System.out.println("Média = "+nota6);
+
+
+
+
+    }
+}
